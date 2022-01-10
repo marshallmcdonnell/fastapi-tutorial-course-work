a8527ebc170af3809fccb02ad369d37bb3e1b84a
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-01-10 12:28:23 -0500

    Completes 05 - Serving HTML with FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from core.config import settings
-
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    version=settings.PROJECT_VERSION
-)
+from apis.general_pages.route_homepage import general_pages_router
 
 
-@app.get("/")
-def hello_api():
-    return {"msg": "Hello API"}
+def include_router(app):
+    app.include_router(general_pages_router)
+
+
+def start_application():
+    app = FastAPI(
+        title=settings.PROJECT_NAME,
+        version=settings.PROJECT_VERSION
+    )
+    include_router(app)
+    return app
+
+app = start_application()
