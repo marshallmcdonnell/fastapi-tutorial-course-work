3f05ae6c5f87bbd3289aa99642ce850603bbbe1c
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-01-12 15:25:53 -0500

    Completes 09 - Creating tables in FastAPI

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "0.0.1"
+
+    SQLITE_DB: str = os.getenv("SQLITE_DB", "test.db")
+    DATABASE_URL: str = f"sqlite:///./{SQLITE_DB}"
 
 
 settings = Settings()
